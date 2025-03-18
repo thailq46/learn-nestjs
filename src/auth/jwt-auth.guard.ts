d47805bd13691afd041c74c41665970e36c25467
thailq <xinphatmutikill@gmail.com>
2025-03-19 01:19:33 +0700

    Enhance JWT authentication: add permission checks in JwtAuthGuard and include user permissions in JwtStrategy validation.

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,7 @@
-import {ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
+import {ExecutionContext, ForbiddenException, Injectable, UnauthorizedException} from '@nestjs/common';
 import {Reflector} from '@nestjs/core';
 import {AuthGuard} from '@nestjs/passport';
+import {Request} from 'express';
 import {IS_PUBLIC_KEY} from 'src/decorator/customize';
 
 @Injectable()
@@ -21,10 +22,23 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err, user, info) {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    const resquest: Request = context.switchToHttp().getRequest();
     // You can throw an exception based on either "info" or "err" arguments
     if (err || !user) {
       throw err || new UnauthorizedException('Token không hợp lệ');
+    }
+    // Check permission
+    const targetMethod = resquest.method;
+    const targetEndpoint = resquest.route.path;
+
+    const permissions = user?.permissions || [];
+
+    const hasPermission = permissions.find(
+      (permission) => targetMethod === permission.method && targetEndpoint === permission.endpoint,
+    );
+    if (!hasPermission) {
+      throw new ForbiddenException('Không có quyền truy cập');
     }
     return user;
   }
--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -2,11 +2,15 @@ import {Injectable} from '@nestjs/common';
 import {ConfigService} from '@nestjs/config';
 import {PassportStrategy} from '@nestjs/passport';
 import {ExtractJwt, Strategy} from 'passport-jwt';
+import {RolesService} from 'src/roles/roles.service';
 import {IUser} from 'src/types/user.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private roleService: RolesService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,13 +18,16 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: IUser) {
-    const {_id, name, email, role} = payload;
+  async validate(user: IUser) {
+    const {_id, name, email, role} = user;
+    // Gán thêm permission vào req.user
+    const userRole = await this.roleService.findOne(role._id);
     return {
       _id,
       name,
       email,
       role,
+      permissions: userRole?.permissions,
     };
   }
 }
